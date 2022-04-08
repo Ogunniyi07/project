ebc942d725e9c6e67c0961a412a1a14cba1f11c2
Ogunniyi07 <ogunniyi.julius12@gmail.com>
2022-04-08 09:41:14 +0100

    Added some code

--- src/App.js
+++ src/App.js
@@ -3,22 +3,59 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <body>
+      <div className="world">
+        <div className="header">
+          <div className="logo">MyTestApp</div>
+        </div>
+        <div className="hero">
+          <div className="title">Watch Something Incredible.</div>
+        </div>
+      </div> 
+      <div className="body">
+        <div className="search">
+          Search
+          <input type="text" className="searchBox" />
+        </div>
+        <div className="boxName">
+          $Movie Category Name
+          <div className="box">
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+          </div>
+        </div>
+        <div className="boxName2">
+          $Movie Category Name
+          <div className="box2">
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+            <div className="box1">$Movie Name</div>
+          </div>
+        </div>
+      </div>
+    </body>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
